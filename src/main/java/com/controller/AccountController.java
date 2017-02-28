7301b82f83868b6862e886a9b0b3d8936d02a819
PeisenXu <75037664@qq.com>
2017-02-28 13:17:34 +0800

    no message

--- src/main/java/com/controller/AccountController.java
+++ src/main/java/com/controller/AccountController.java
@@ -1,7 +1,27 @@
 package com.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by Sena on 2017/2/28.
  */
+@Controller
+@RequestMapping("/api/v1/account")
 public class AccountController {
+
+    /**
+     * Healthy check url
+     */
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(value = "/healthy", method = RequestMethod.GET)
+    public ModelAndView healthyCheck() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+
 }
