3de22b3152ce1aef26e630ee8cb75b53e83e2166
PeisenXu <75037664@qq.com>
2017-03-01 12:49:48 +0800

    配置修改

--- src/main/java/com/sena/mapper/AccountUserMapper.java
+++ src/main/java/com/sena/mapper/AccountUserMapper.java
@@ -15,8 +15,6 @@ public class AccountUserMapper implements RowMapper<UserEntity> {
     private String HASHPASSWORD = "HashPassword";
     private String EMAIL = "Email";
 
-
-
     public UserEntity mapRow(ResultSet rs, int i) throws SQLException {
         UserEntity user = new UserEntity();
         user.setId(rs.getInt(ID));
