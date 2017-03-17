0e68c2d1e82fe58d7541d34cd7d0e0bd5e46db4e
PeisenXu <75037664@qq.com>
2017-03-17 18:36:41 +0800

    no message

--- src/main/java/com/sena/service/impl/EmailServiceImpl.java
+++ src/main/java/com/sena/service/impl/EmailServiceImpl.java
@@ -31,13 +31,13 @@ public class EmailServiceImpl implements EmailService {
     // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-    public static String myEmailAccount = "peisenxu@qq.com";
-    public static String myEmailPassword = "hlblaevabngvcbdb";
+    public static String myEmailAccount = "email.prompt@foxmail.com";
+    public static String myEmailPassword = "pviddftxnfcebhae";
     public static String myEmailName = "Confidential";
 
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     // 网易163邮箱的 SMTP 服务器地址为: smtp.163.com
-    public static String myEmailSMTPHost = "smtp.qq.com";
+    public static String myEmailSMTPHost = "smtp.exmail.qq.com";
 
     // 收件人邮箱（替换为自己知道的有效邮箱）
     public static String receiveMailAccount = "shadowred@foxmail.com";
