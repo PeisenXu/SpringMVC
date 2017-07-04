712880bed73b6621bca1cd9a995a1132aaf4e70b
Sena <admin@xupeisen.com>
2017-07-04 15:06:17 +0800

    no message

--- src/main/java/com/sena/dao/EmailDao.java
+++
@@ -1,8 +0,0 @@
-package com.sena.dao;
-
-/**
- * Created by Sena on 2017/4/12.
- */
-public interface EmailDao {
-    void createEmailRecord (String userName, String emailTo, String fileName, String fileUrl, String subject, String message, String status);
-}
