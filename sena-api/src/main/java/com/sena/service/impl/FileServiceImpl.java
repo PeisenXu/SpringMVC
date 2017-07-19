181887fa6fd36e31ffa550701ea102241982317a
Sena <admin@xupeisen.com>
2017-07-19 16:16:16 +0800

    no message

--- sena-api/src/main/java/com/sena/service/impl/FileServiceImpl.java
+++ sena-api/src/main/java/com/sena/service/impl/FileServiceImpl.java
@@ -5,7 +5,9 @@ import com.sena.service.FileService;
 import com.sena.util.FileUtil;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -29,12 +31,20 @@ public class FileServiceImpl implements FileService {
             conn.setConnectTimeout(20 * 1000);
             //防止屏蔽程序抓取而返回403错误
             conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-
+            conn.setConnectTimeout(5 * 1000);
+            //conn.setRequestMethod("utf-8");
+            conn.setRequestProperty("contentType", "utf-8");
             //得到输入流
             InputStream inputStream = conn.getInputStream();
-            //获取自己数组
-            byte[] getData = FileUtil.readInputStream(inputStream);
-            return new String(getData);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = in.readLine()) != null){
+                buffer.append(line);
+            }
+            String str = buffer.toString();
+            return str;
         } catch (Exception e) {
             e.printStackTrace();
             return "";
